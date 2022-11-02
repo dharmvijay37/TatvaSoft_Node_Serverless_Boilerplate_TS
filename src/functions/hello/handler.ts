5470d84397949d360b7b877698e41f2e34fde3d6
DharmvijayPatel <dharmvijay.patel@connexionltd.com>
2022-11-02 18:05:07 +0530

    minor code refactore

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,11 +1,11 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
+import { successResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  return formatJSONResponse({
+  return successResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
     event,
   });
--- src/tests/libs/api-gateway.test.ts
+++ src/tests/libs/api-gateway.test.ts
@@ -1,21 +1,24 @@
 import { describe, it } from "mocha";
 import { expect } from 'chai';
 // import chaiAsPromised from 'chai-as-promised';
-import { formatJSONResponse } from '../../libs/api-gateway';
+import { successResponse } from '../../libs/api-gateway';
 
 describe('apiGateway', () => {
-    const sampleDiagnosticNew = {
-        updateTimeStamp: new Date().toTimeString(),
-        Diagnostic: [],
-        Location: { a: 20 },
-        TripHBHA: { b: 20 }
-      }
-    
-    describe('formatJSONResponse()', () => {
-        it('should return with valid response', () => {
-            expect(formatJSONResponse(sampleDiagnosticNew)).to.exist
-          })
-      
+  const sampleRecords = {
+    updateTimeStamp: new Date().toTimeString(),
+    Diagnostic: [],
+    Location: { a: 20 },
+    TripHBHA: { b: 20 }
+  }
+  
+  describe('formatJSONResponse()', () => {
 
+    it('should return with valid response', () => {
+      expect(successResponse(sampleRecords)).to.exist
     });
+
+    it('should return with valid response', () => {
+      expect(successResponse(sampleRecords)).to.exist
+    })
+  });
 });
